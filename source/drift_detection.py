4cab1e571f1eb7fd0266f4869c2392f5aaf3a487
Rafael Gaspar <rafaelgaspar@hotmail.com.br>
2020-12-10 23:28:01 -0300

    Adicionando comentários para detecção e localização

--- source/drift_detection.py
+++ source/drift_detection.py
@@ -5,11 +5,22 @@ import scipy
 import scipy.stats
 
 
-def get_metrics(drifts, resp, window_size=100, verbose=False):
+def get_metrics(drifts: list, resp: list, window_size=100, verbose=False) -> dict:
     """
-    Precision, Recall, F1 Score and Delay
-    for the drift predictions
+    Given the drifts predicted and the ground truth, calculates binary classification metrics 
+    (Precision, Recall, F1 Score) and delay of detection. We consider a drift to be detected
+    correctly when it has been found within 2 * window_size after its true index. 
+
+    Parameters:
+        drifts (list): List with the index of the detected drifts
+        resp (list): List with the index of the ground truth drifts
+        window_size (int): Size of the trace clustering window used to consider the delay.
+        verbose (bool): Whether to print results
+
+    Returns:
+        dict: dictionary with the value of each metric
     """
+    # Initialize metrics with 0
     precision = 0
     recall = 0
     tp = 0
@@ -18,17 +29,19 @@ def get_metrics(drifts, resp, window_size=100, verbose=False):
     resp_ = resp.copy()
     predicted = [0 for x in resp_]
     
+    # Transforms the window_size into a vector with the size of the drifts found
     if isinstance(window_size, int):
         window_size = np.repeat(window_size, len(drifts))
     
+    # Iterates over all drifts found and to all ground truths drifts
     for i in range(len(drifts)):    
-        for j in range(len(resp_)):
-            # print(drifts[i], resp_[j], drifts[i] - resp_[j])
-            
+        for j in range(len(resp_)):            
+            # check if the drift found is within 2 * window_size after its true index
             if 0 <= drifts[i] - resp_[j] <= 2 * window_size[i]:
                 if verbose:
                     print((drifts[i], drifts[i] + window_size[i], resp_[j]))
-                    
+                
+                # drift found correctly
                 delay += drifts[i] - resp_[j]
                 tp += 1
                 resp_[j] = np.inf
@@ -37,7 +50,7 @@ def get_metrics(drifts, resp, window_size=100, verbose=False):
     
     if len(drifts) > 0:
         precision = tp/len(drifts)
-    
+
     if len(resp_) > 0:
         recall = tp/len(resp_)
         
@@ -61,33 +74,50 @@ def get_metrics(drifts, resp, window_size=100, verbose=False):
     }
 
 
-def exp_mw_avg_std(value, a):
-    mrt_array = np.array(value)
-    M = len(mrt_array)
-    weights = (1-a)**np.arange(M-1, -1, -1) # This is reverse order to match Series order
-    ewma = sum(weights * mrt_array) / sum(weights)
-    bias = sum(weights)**2 / (sum(weights)**2 - sum(weights**2))
-    ewmvar = bias * sum(weights * (mrt_array - ewma)**2) / sum(weights)
-    ewmstd = np.sqrt(ewmvar)
-    return ewma, ewmstd
-
-
 def detect_concept_drift(
     df, var_ref, rolling_window=5, std_tolerance=3, min_tol=0.0025, verbose=False
 ):
+    """
+        Performs the drift detection algorithm. Based on the features measured from 
+        tracking the evolution of the trace clustering over the windows, estimate 
+        tolerance boundaries to detect a drift when the feature value measured at the
+        current index lies outside of the boundaries. 
+
+        Parameters:
+            df (pd.DataFrame): DataFrame of features from tracking the trace clusterings
+            var_ref (str): Name of the feature to apply the algorithm. It has to be present
+                in the df.columns
+            rolling_window (int): The number of rolling windows to consider when estimating
+                the tolerance boundaries and detecting the drifts. It smooths the analyzed 
+                feature to reduce false positive detections due to noise. 
+            std_tolerance (int): Number of times the rolling standard deviation used to 
+                the tolerance boundaries. A higher value provides higher tolerance and 
+                lower sensitivity to drifts.
+            min_tol (float [0.0 - 1.0]): Number of times the rolling average to calculate a
+                minimum tolerance boundaries. Useful for not detecting false positives in more 
+                stable feature values when the tolerance could be too little.
+            verbose (bool): Whether to print the index of drifts as they are found
+        
+        Returns:
+            list: List of index of the detected drifts
+            dict: Dictionary with the rolling average, lowers and uppers boundaries 
+                to assist plotting
+    """
+    # Initialize variables
     window_buffer = []
     drifts = []
     mean = None
     std = None
 
+    # Lists to keep the rolling average and the lower and upper boundaries to 
+    # be returned at the end of the execution of this method to support the plots
     lowers = []
     uppers = []
-    if use_median:
-        means = df[var_ref].rolling(window=rolling_window).median().values.tolist()
-    else:    
-        means = df[var_ref].rolling(window=rolling_window).mean().values.tolist()
+    means = df[var_ref].rolling(window=rolling_window).mean().values.tolist()
 
+    # Iterates over the values
     for i, row in df.iterrows():
+        # If the rolling window is of the desired size
         if len(window_buffer) < rolling_window:
             window_buffer.append(row[var_ref])
             lowers.append(np.nan)
@@ -95,9 +125,11 @@ def detect_concept_drift(
 
         else:
             if mean is not None:
+                # To avoid errors in multiplication with 0
                 if mean == 0:
                     mean == 1
 
+                # Calculates tolerance boundaries considering the rolling mean and std
                 expected_lower = min(
                     mean - (std_tolerance * std),
                     (1 - min_tol) * mean
@@ -107,9 +139,11 @@ def detect_concept_drift(
                     (1 + min_tol) * mean
                 )
 
+                # Adds into the list to return in the end
                 lowers.append(expected_lower)
                 uppers.append(expected_upper)
 
+                # Checks whether the current value lies outside the tolerance boundaries
                 if expected_lower > row[var_ref] or row[var_ref] > expected_upper:
                     if verbose:
                         print(i, expected_lower, expected_upper, row[var_ref])
--- source/drift_localization.py
+++ source/drift_localization.py
@@ -1,18 +1,36 @@
 import pandas as pd
 from scipy.optimize import linear_sum_assignment
+from scipy.spatial import distance
 
-def localize_drift(old_centroids, new_centroids, col_names):
-    if isinstance(old_centroids, pd.Series):
-        old_centroids = old_centroids.mean(axis=0)
+def localize_drift(before_drift_centroids, after_drift_centroids, col_names):
+    """
+        Performs the drift localization method based on the MSE 
+        between centroids from the windows before and after the drift.
+
+        The centroid matching step is solved using the linear sum assignment
+        Hungarian method proposed in `Harold W. Kuhn. The Hungarian Method for the
+        assignment problem. Naval Research Logistics Quarterly, 2:83-97, 1955`
+
+        Parameters:
+            before_drift_centroids (pd.DataFrame): Centroids before the drift
+            after_drift_centroids (pd.DataFrame): Centroids after the drift
+            col_names (list): List with the names of the columns relating to 
+                each dimension of the traces vector representation (activities 
+                or transitions)
+            
+        Returns:
+            pd.DataFrame: DataFrame with the MSE of each dimension 
+    """
+    if isinstance(before_drift_centroids, pd.Series):
+        before_drift_centroids = before_drift_centroids.mean(axis=0)
     
-    if isinstance(new_centroids, pd.Series):
-        new_centroids = new_centroids.mean(axis=0)
-        
-    r, c = linear_sum_assignment(distance.cdist(old_centroids, new_centroids))
-    lut = c
+    if isinstance(after_drift_centroids, pd.Series):
+        after_drift_centroids = after_drift_centroids.mean(axis=0)
+
+    _ , lut = linear_sum_assignment(distance.cdist(before_drift_centroids, after_drift_centroids))
     
     resp = pd.DataFrame([
-        ((old_centroids - new_centroids[lut]) ** 2).mean(axis=0)
+        ((before_drift_centroids - after_drift_centroids[lut]) ** 2).mean(axis=0)
     ], columns=col_names).transpose()
     
     return resp
